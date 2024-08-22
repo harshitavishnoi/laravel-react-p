32167bb414131551a45ce5222cc92f851ae3cbd8
Harshita Vishnoi <Harshita.Vishnoi@in.niit.com>
2024-08-22 14:04:42 +0530

    first commit

---
+++ resources/js/Components/HelloReact.tsx
@@ -0,0 +1,11 @@
+import react from "react";
+import ReactDOM from "react-dom";
+
+export default function HelloReact(){
+    return (
+        <>
+            <h1>Hello harshita</h1>
+        </>
+    )
+}
+
